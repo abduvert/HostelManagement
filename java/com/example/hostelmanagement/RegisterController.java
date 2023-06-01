3d156e3aa4cb317b682217db0569bc5ce6a408ec
RidaAbid7 <ridaabid767@gmail.com>
2023-06-01 20:33:58 +0500

    register

--- java/com/example/hostelmanagement/RegisterController.java
+++ java/com/example/hostelmanagement/RegisterController.java
@@ -1,10 +1,10 @@
 package com.example.hostelmanagement;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
@@ -14,7 +14,7 @@ import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.sql.ResultSet;
-import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static com.example.hostelmanagement.HelloApplication.statement;
 
@@ -33,12 +33,20 @@ public class RegisterController {
     @FXML
     public Button done;
     @FXML
-    public TextField fname, lname, dob, semester, cgpa, degree, batch, PhnCode, PhnNo, CNICcode, CNICno, email, religion, Hno, town;
-
-    public TextField security;
+    public TextField fname, lname, dob, semester, cgpa, degree, batch, Phncode, PhnNo, CNICcode, CNICno, email, religion, Hno, town, security;
+    @FXML
     public PasswordField password;
-
-   //public ComboBox<String>;
+    @FXML
+   public ComboBox<String> comboBox = new ComboBox<>();
+    public String newStId;
+    public String selected_city;
+    @FXML
+    public TextField G_fname, G_lname, G_relation, G_PHNcode, G_PHNno, G_CNICcode, G_CNICno, G_email, G_religion, G_Hno, G_town;
+    @FXML
+    public ComboBox<String> comboBox2 = new ComboBox<>();
+    public String G_city;
+    @FXML
+    public TextField v_fname, v_lname, v_relation, v_PHNcode, v_PHNno,  v_CNICcode, v_CNICno;
     @FXML
     protected void Register(ActionEvent event) throws IOException {
         FXMLLoader reg1 = new FXMLLoader(getClass().getResource("RegisterPage.fxml"));
@@ -48,9 +56,28 @@ public class RegisterController {
     }
 
     @FXML
+    public void initialize(){
+        ObservableList<String> items = FXCollections.observableArrayList("Arizona", "Auburn", "Boston",
+                "Chicago", "California", "Florence", "Florida", "Greenville", "Kansas", "New York", "Ohio", "Portland", "Seattle");
+        comboBox.setItems(items);
 
+        ObservableList<String> items2 = FXCollections.observableArrayList("Arizona", "Auburn", "Boston",
+                "Chicago", "California", "Florence", "Florida", "Greenville", "Kansas", "New York", "Ohio", "Portland", "Seattle");
+        comboBox2.setItems(items2);
+    }
+
+    @FXML
+    protected void Show(){
+        selected_city = comboBox.getValue();
+        comboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("Selected value: " + newValue);
+            System.out.println("2." + selected_city);
+            selected_city = newValue;
+        });
+    }
+
+    @FXML
     protected void Next1(ActionEvent backevent) throws IOException {
-
         try{
             // Step 1: Retrieve the maximum st_id value
             String maxStIdQuery = "SELECT MAX(st_id) FROM Student";
@@ -60,15 +87,17 @@ public class RegisterController {
 
             // Step 2: Generate the new st_id
             int numericPart = Integer.parseInt(maxStId.substring(1)); // Extract numeric part
-            String newStId = "S" + String.format("%03d", numericPart + 1); // Increment and format
+
+            newStId = "S" + String.format("%03d", numericPart + 1); // Increment and format
 
             // Step 3: Insert the new record
             String InsertQuery = "insert into Student(st_id, batch, degree, st_firstName, st_lastName, st_PHNcode, st_PHNno, st_email, st_dob, st_CNICcode, st_CNICno, religion, st_city, st_town, s_houseNo, cgpa, st_password, st_SecurityQuestion) values " +
-                    "('" + newStId + "' , '" + batch.getText() + "' , '" + degree.getText() + "' , '" + fname.getText() + "' , '" + lname.getText() + "' , " + PhnCode.getText() + ", " + PhnNo.getText() + ", '" + email.getText() + "' , '" + dob.getText() + "' , " + CNICcode.getText() + ", " + CNICno.getText() + ", '" + religion.getText() + "' , '" + "CITY TO BE INSERTED" + "' , '" + town.getText() + "' , '" + Hno.getText() + "' , " + cgpa.getText() + ", '" + password.getText() +"' , '" + security.getText() + "'";
+                    "('" + newStId + "' , '" + batch.getText() + "' , '" + degree.getText() + "' , '" + fname.getText() + "' , '" + lname.getText() + "' , " + Phncode.getText() + ", " + PhnNo.getText() + ", '" + email.getText() + "' , '" + dob.getText() + "' , "
+                    + CNICcode.getText() + ", " + CNICno.getText() + ", '" + religion.getText() + "' , '" + selected_city + "' , '" + town.getText() + "' , '" + Hno.getText() + "' , " + cgpa.getText() + ", '" + password.getText() +"' , '" + security.getText() + "')";
 
-            statement.executeUpdate(InsertQuery);
+            HelloApplication.statement.executeQuery(InsertQuery);
         } catch (Exception e){
-            e.getMessage();
+            System.out.println( e.getMessage());
             e.printStackTrace();
         }
 
@@ -76,18 +105,41 @@ public class RegisterController {
         stage1.setScene(new Scene(reg2.load()));
 
     }
-
-
     @FXML
-
+    protected void Show2(){
+        selected_city = comboBox.getValue();
+        comboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("Selected value: " + newValue);
+            G_city = newValue;
+        });
+    }
+    @FXML
     protected void Next2(ActionEvent backevent) throws IOException {
 
-        FXMLLoader reg2 = new FXMLLoader(getClass().getResource("Register3.fxml"));
+        try{
+            // Retrieve the maximum st_id value
+            String maxStIdQuery = "SELECT MAX(st_id) FROM Student";
+            ResultSet resultSet = statement.executeQuery(maxStIdQuery);
+            resultSet.next();
+            String maxStId = resultSet.getString(1);
+            System.out.println("in 2nd st_id: " + maxStId);
+
+
+        String InsertQuery2 = "insert into Guardian(st_id, g_firstName, g_lastname, g_CNICcode, g_CNICno, g_relation,g_PHNcode, g_PHNno, g_city, g_town, g_houseNo, g_email) values " +
+                "('" + maxStId + "' , '" + G_fname.getText() + "' , '" + G_lname.getText() + "' ," + G_CNICcode.getText() + "," + G_CNICno.getText() + ", '" + G_relation.getText() + "' , " + G_PHNcode.getText() + ", " + G_PHNno.getText() + ", '" + G_city + "' , '"
+                + G_town.getText() + "' , '" + G_Hno.getText() + "' , '" + G_email.getText() + "')";
+
+        HelloApplication.statement.executeQuery(InsertQuery2);
+    } catch (Exception e){
+            System.out.println( e.getMessage());
+            e.printStackTrace();
+    }
+
+    FXMLLoader reg2 = new FXMLLoader(getClass().getResource("Register3.fxml"));
         stage1.setScene(new Scene(reg2.load()));
     }
 
     @FXML
-
     protected void Cancel1(ActionEvent backevent) throws IOException {
 
         FXMLLoader reg2 = new FXMLLoader(getClass().getResource("RegisterPage.fxml"));
